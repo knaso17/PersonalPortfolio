b1c6dc31e53292b253cf64b2c0ed6b7caedf8a26
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-07-03 13:59:01 -0500

    add maifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,32 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: [
+    "gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp",
+    "gatsby-plugin-fontawesome-css",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Montserrat`, //basic font
+          `Prosto One` // logo font/ headers
+        ],
+        display: 'swap'
+      }
+    },
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
