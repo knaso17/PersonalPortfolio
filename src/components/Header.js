1bd3d9434a0ee97b98ed1ce77b7cb740894e94b8
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-07-05 20:36:26 -0500

    added header content

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,29 @@ import "../styles/styles.scss";
 
 export default function About() {
   return (
-    <h1>header</h1>
+    <header className="header">
+          <div className="header__logo" >
+            <section className="header__imgContainer">
+              <StaticImage
+                src="../images/KNLogo.png"
+                alt="KN logo"
+                placeholder="blurred"
+                layout="fixed"
+                style={{
+                  width: "100px",
+                  height: "100px",
+                }}
+              />
+              </section>
+              <p className="header_logoText">Krista Naso</p>
+            </div>
+          <nav className="header__nav">
+            <a href="#top" className="nav__link">Home</a>
+            <a href="#about" className="nav__link">About</a>
+            <a href="#tech" className="nav__link">Tech</a>
+            <a href="#projects" className="nav__link">Projects</a>
+            <a href="#recs" className="nav__link">Recs</a>
+          </nav>
+      </header>
   );
 }
