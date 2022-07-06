badebdd81e683b56d4e5a7dfa9ae0d3a9482d092
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-07-05 20:30:20 -0500

    added header component

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "../styles/styles.scss";
+
+export default function About() {
+  return (
+    <h1>header</h1>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
-import * as React from "react"
-import "../styles/styles.scss"
+import * as React from "react";
+import "../styles/styles.scss";
 
-import About from "../components/About"
+import About from "../components/About";
+import Header from "../components/Header";
 
 const IndexPage = () => {
   return (
-    <main >
-      <title>Krista Naso's Portfolio</title>
-      <h1>Krista Naso's Portfolio</h1>
-      <About/>
-    </main>
+    <React.Fragment>
+      <Header />
+      <main >
+        <title>Krista Naso's Portfolio</title>
+        <h1>Krista Naso's Portfolio</h1>
+        <About/>
+      </main>
+    </React.Fragment>
   )
 }
 
