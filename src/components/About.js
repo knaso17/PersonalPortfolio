ad928c86e876c6fbac0be6fe317bd0cd4da7d30c
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-08-11 20:17:19 -0500

    create list for about section

--- src/components/About.js
+++ src/components/About.js
@@ -10,30 +10,44 @@ export default function About() {
   return (
       <div className="about" id="about">
           <h2 className="about__header">About Me</h2>
-          <p className="about__para">
-            <FontAwesomeIcon icon={faHand} label={"hello"}/>
-            Hi, I'm Krista! I'm a software engineer from the Chicago-land area.
-          </p>
-          <p className="about__para">
-            <FontAwesomeIcon icon={faBusinessTime} label={"oldWork"}/>
-            I used to be an eCommercer marketer, but I wasn't growing in that role…
-          </p>
-          <p className="about__para">
-            <FontAwesomeIcon icon={faCode} label={"code"}/>
-            So I learned to code! Partially on my own and then with the help of an immersive program.
-          </p>
-          <p className="about__para">
-            <FontAwesomeIcon icon={faLaptopCode} label={"newWork"}/>
-            Now I work full-time as front-end web developer!
-          </p>
-          <p className="about__para">
-            <FontAwesomeIcon icon={faBookOpen} label={"learning"}/>
-            I'm still learning all the time and can't wait to see what new opportunities come my way!
-          </p>
-          <p className="about__para">
-            <FontAwesomeIcon icon={faComments} label={"contact"}/>
-            To learn more, or to contact me, click one of the icons below!
-          </p>
+          <ul className="about__list">
+            <li className="about__listItem">
+              <FontAwesomeIcon icon={faHand} label={"hello"}/>
+              <p className="about__para">
+                Hi, I'm Krista! I'm a software engineer from the Chicago-land area.
+              </p>
+            </li>
+            <li className="about__listItem">
+              <FontAwesomeIcon icon={faBusinessTime} label={"oldWork"}/>
+              <p className="about__para">
+                I used to be an eCommercer marketer, but I wasn't growing in that role…
+              </p>
+            </li>
+            <li className="about__listItem">
+              <FontAwesomeIcon icon={faCode} label={"code"}/>
+              <p className="about__para">
+                So I learned to code! Partially on my own and then with the help of an immersive program.
+              </p>
+            </li>
+            <li className="about__listItem">
+              <FontAwesomeIcon icon={faLaptopCode} label={"newWork"}/>
+              <p className="about__para">
+                Now I work full-time as front-end web developer!
+              </p>
+            </li>
+            <li className="about__listItem">
+              <FontAwesomeIcon icon={faBookOpen} label={"learning"}/>
+              <p className="about__para">
+                I'm still learning all the time and can't wait to see what new opportunities come my way!
+              </p>
+            </li>
+            <li className="about__listItem">
+              <FontAwesomeIcon icon={faComments} label={"contact"}/>
+              <p className="about__para">
+                To learn more, or to contact me, click one of the icons below!
+              </p>
+            </li>
+          </ul>
           <div className="about__link-container">
             <a href="https://www.linkedin.com/in/krista-naso/"
               target="_blank"
