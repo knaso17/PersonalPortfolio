53385a1c1ea24120c9a2d821d69844b72c363233
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-08-14 10:42:39 -0500

    test read more component

--- src/components/Recommendations.js
+++ src/components/Recommendations.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../styles/styles.scss";
 
-export default function Recommendations() {
+const ReadMore = ({ children }) => {
+  const text = children;
+  const [isReadMore, setIsReadMore] = useState(true);
+  const toggleReadMore = () => {
+    setIsReadMore(!isReadMore);
+  };
+
+  return (
+    <p className="text">
+      {isReadMore ? text.slice(0, 150) : text}
+      <span onClick={toggleReadMore} className="read-or-hide">
+        {isReadMore ? "...read more" : " show less"}
+      </span>
+    </p>
+  );
+};
+
+const Recommendations = () => {
   return (
     <div className="recs__container"id="recs">
       <h2 className="recs__header"> Recs </h2>
         <section>
           <div>
-            <p className="recs__content">
+            <ReadMore>
               "Krista was the true MVP of our Grace Hopper Capstone project, Leashed. Leashed is a cross-platform mobile app that allows users to create profiles for their dogs to connect with local dogs through matching and realtime chat. I worked with Krista for three weeks on the development of this app and no matter what the challenge was, Krista was always the first one to offer assistance in debugging and pair programming. On top of her exceptional collaboration skills, she is also tremendously reliable as an independent worker. She voluntarily took on tasks such as user authentication, implementing the navigation stack/bar, and persisting global user context and was able to push up deliverables in a timely manner which kept our team running like a well-oiled machine. I can attest from first-hand experience that Krista would be an impactful asset to any team."
-            </p>
+            </ReadMore>
             <p className="recs__name">- Ashley Tran</p>
             <p className="recs__relationship">
               {`  Ashley worked with Krista on the same team`}
@@ -38,3 +55,5 @@ export default function Recommendations() {
       </div>
   );
 }
+
+export default Recommendations;
